ec895e30232b1f2af078056be6cc7a3ad3f44106
Andrey Aratov <beg431@gmail.com>
2024-01-07 23:33:43 +0700

    chore: vite pwa plugin config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,5 +11,70 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+      },
+      manifest: {
+        short_name: 'todo.it',
+        name: 'todo.it – todo app with features',
+        description: 'Another todo app but with features',
+        start_url: '.',
+        display: 'standalone',
+        orientation: 'portrait',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        categories: ['productivity', 'lifestyle', 'utilities'],
+        icons: [
+          {
+            src: 'favicon-16x16.png',
+            sizes: '16x16',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+          {
+            src: 'favicon-32x32.png',
+            sizes: '32x32',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+          {
+            src: 'favicon.ico',
+            sizes: '48x48',
+            type: 'image/x-icon',
+          },
+          {
+            src: 'android-chrome-144x144.png',
+            type: 'image/png',
+            sizes: '144x144',
+            purpose: 'any',
+          },
+          {
+            src: 'android-chrome-192x192.png',
+            type: 'image/png',
+            sizes: '192x192',
+            purpose: 'maskable',
+          },
+          {
+            src: 'android-chrome-512x512.png',
+            type: 'image/png',
+            sizes: '512x512',
+            purpose: 'maskable',
+          },
+          {
+            src: 'apple-touch-icon.png',
+            type: 'image/png',
+            sizes: '180x180',
+            purpose: 'maskable',
+          },
+        ],
+      },
+      devOptions: {
+        enabled: false,
+      },
+    }),
+  ],
 });
