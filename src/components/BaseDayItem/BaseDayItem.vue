d4bcf5d1aeb3cf77ab3a1b69f8545154f53c5546
Andrey Aratov <beg431@gmail.com>
2024-01-14 12:20:08 +0700

    chore: calendar

---
+++ src/components/BaseDayItem/BaseDayItem.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="flex items-center">
+    <div v-if="day.isCurrent">>>>></div>
+    {{ day.number }}
+    ({{ day.name }})
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { IDay } from '@/types';
+
+interface IProps {
+  day: IDay;
+}
+
+defineProps<IProps>();
+</script>
--- src/components/layouts/BaseLayout/BaseLayout.vue
+++ src/components/layouts/BaseLayout/BaseLayout.vue
@@ -2,8 +2,17 @@
   <BaseHeader />
 
   <main class="container mx-auto px-3 pb-10">
-    <div class="flex flex-col lg:flex-row">
-      CONTENT
+    <div class="p-12">
+      <p>3000</p>
+      <small>last 6 months</small>
+    </div>
+
+    <div class="flex flex-col">
+      <div v-for="month in months" :key="month.id">
+        {{ month.name }}
+
+        <BaseDayItem v-for="day in getDaysByMonthId(month.id)" :key="day.id" :day="day" />
+      </div>
     </div>
   </main>
 
@@ -11,6 +20,13 @@
 </template>
 
 <script setup lang="ts">
+import { storeToRefs } from 'pinia';
+import { useCalendarStore } from '@/stores';
 import BaseHeader from '@/components/layouts/partials/BaseHeader/BaseHeader.vue';
 import BaseFooter from '@/components/layouts/partials/BaseFooter/BaseFooter.vue';
+import BaseDayItem from '@/components/BaseDayItem/BaseDayItem.vue';
+
+const calendarStore = useCalendarStore();
+const { months } = storeToRefs(calendarStore);
+const { getDaysByMonthId } = calendarStore;
 </script>
