18388c4b4fd714bd47f19c5ff807d7da87fec55c
Andrey Aratov <beg431@gmail.com>
2024-01-15 23:26:39 +0700

    chore: add button component

--- src/components/layouts/BaseLayout/BaseLayout.vue
+++ src/components/layouts/BaseLayout/BaseLayout.vue
@@ -8,7 +8,7 @@
       <BaseExpenseList />
 
       <div class="container fixed left-0 right-0 bottom-2 w-full px-4 mx-auto">
-        <div class="flex items-center rounded-xl overflow-hidden border border-slate-300">
+        <div class="flex items-center rounded-xl overflow-hidden bg-white border border-slate-300 shadow-lg">
           <BaseInput
             v-model="expense"
             id="expense-input"
@@ -16,6 +16,14 @@
             placeholder="Enter expense"
             class="border-r border-slate-300"
           />
+
+          <BaseButton
+            title="Add expense"
+          >
+            <template #leftIcon>
+              <PlusCircleIcon class="w-6 h-6" />
+            </template>
+          </BaseButton>
         </div>
       </div>
     </BasePanel>
@@ -24,11 +32,13 @@
 
 <script setup lang="ts">
 import { ref } from 'vue';
+import { PlusCircleIcon } from '@heroicons/vue/24/outline';
 import BaseHeader from '@/components/layouts/partials/BaseHeader/BaseHeader.vue';
 import BasePanel from '@/components/ui/BasePanel/BasePanel.vue';
 import BaseStats from '@/components/BaseStats/BaseStats.vue';
 import BaseExpenseList from '@/components/BaseExpenseList/BaseExpenseList.vue';
 import BaseInput from '@/components/ui/controls/BaseInput/BaseInput.vue';
+import BaseButton from '@/components/ui/controls/BaseButton/BaseButton.vue';
 
 const expense = ref('');
 </script>
---
+++ src/components/ui/controls/BaseButton/BaseButton.vue
@@ -0,0 +1,29 @@
+<template>
+  <button
+    class="flex items-center justify-center px-3 py-2 bg-sky-400 text-white focus:outline-none"
+    :type="type"
+    :title="title"
+    @click="clickHandler"
+  >
+    <slot name="leftIcon" />
+    <slot name="text" />
+    <slot name="rightIcon" />
+  </button>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  type?: 'button' | 'submit' | 'reset';
+  title?: string;
+}
+
+withDefaults(defineProps<Props>(), {
+  type: 'button',
+});
+
+const emits = defineEmits(['click']);
+
+const clickHandler = () => {
+  emits('click');
+};
+</script>
