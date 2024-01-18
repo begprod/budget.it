8076950eaa2581efe7269a979e7e655f72654db2
Andrey Aratov <beg431@gmail.com>
2024-01-18 12:29:35 +0700

    chore: update sidebar

--- src/components/layouts/partials/BaseSidebar/BaseSidebar.vue
+++ src/components/layouts/partials/BaseSidebar/BaseSidebar.vue
@@ -1,8 +1,13 @@
 <template>
-  <div v-if="isOpen" class="fixed top-0 right-0 bottom-0 left-0 w-full bg-slate-500 z-[101]">
-    <slot name="currencies" />
-    <slot name="daily" />
-    <button @click="toggle">close</button>
+  <div
+    v-if="isOpen"
+    class="fixed top-0 right-0 bottom-0 left-0 w-auto bg-slate-500/90 z-[101]"
+  >
+    <div class="relative top-1/3 p-5 h-full rounded-t-2xl bg-white">
+      <slot name="currencies" />
+      <slot name="daily" />
+      <button @click="toggle">close</button>
+    </div>
   </div>
 </template>
 
