eea854c480b302c4bd5dcd30c24a12b3e32fb3f4
Andrey Aratov <beg431@gmail.com>
2024-01-26 23:04:13 +0700

    chore: add BaseExpense component

---
+++ src/components/BaseExpense/BaseExpense.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="flex flex-col items-center">
+    <div class="text-xs text-slate-300">{{ createdAt }}</div>
+    <div class="flex items-baseline py-1 px-3 border border-sky-400 rounded-2xl">
+      <div class="text-sm text-slate-700 leading-6">{{ value }}</div>
+      <div class="ml-1 text-xs leading-5">{{ currency }}</div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+interface IProps {
+  createdAt: string;
+  value: string | number;
+  currency: string;
+}
+
+defineProps<IProps>();
+
+const emit = defineEmits(['click', 'delete']);
+</script>
