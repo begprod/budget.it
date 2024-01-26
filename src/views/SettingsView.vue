a77ec78127326887771db6c4abc5b79f9a392924
Andrey Aratov <beg431@gmail.com>
2024-01-26 23:32:25 +0700

    chore: update setting page

--- src/views/SettingsView.vue
+++ src/views/SettingsView.vue
@@ -3,51 +3,57 @@
     <div class="mb-5 text-2xl text-slate-700 font-bold">Settings</div>
 
     <div class="mb-10">
-      <div class="mb-2">
-        <div class="mb-3 text-slate-500">Default currency</div>
+      <div class="mb-3 text-slate-500">Daily budget</div>
 
-        <div class="flex flex-wrap gap-1">
-          <BaseLabel
-            v-for="currency in currencies"
-            :key="currency.name"
-            :id="currency.name"
-            :label="currency.name"
-            :value="currency.name"
-            :is-selected="currency.isActive"
-            :is-default="currency.isDefault"
-            name="currencies"
-          />
-        </div>
-      </div>
-      <div class="grid grid-cols-12 mt-5">
+      <div class="grid grid-cols-12">
         <BaseInput
-          id="currency-input"
-          v-model="newCurrency"
-          type="text"
-          placeholder="Add new currency"
+          id="daily-input"
+          v-model="dailyBudgetValue"
+          type="number"
+          placeholder="Set daily budget"
           class="col-span-10 rounded-r-none"
         />
-        <BaseButton class="col-span-2 border-l-0 rounded-l-none" @click="saveNewCurrency(newCurrency)">
+        <BaseButton
+          class="col-span-2 border-l-0 rounded-l-none"
+          @click="setDailyBudget(dailyBudgetValue)"
+        >
           <template #text>
-            <PlusIcon class="w-6 h-6" />
+            <CheckIcon class="w-6 h-6" />
           </template>
         </BaseButton>
       </div>
     </div>
 
-    <div class="mb-3 text-slate-500">Daily budget</div>
+    <div class="mb-2">
+      <div class="mb-3 text-slate-500">Default currency</div>
 
-    <div class="grid grid-cols-12">
+      <div class="flex flex-wrap gap-1">
+        <BaseCurrencyGroupItem
+          v-for="currency in currencies"
+          :key="currency.name"
+          :id="currency.name"
+          :label="currency.name"
+          :value="currency.name"
+          :is-selected="currency.isActive"
+          :is-default="currency.isDefault"
+          name="currencies"
+        />
+      </div>
+    </div>
+    <div class="grid grid-cols-12 mt-3">
       <BaseInput
-        id="daily-input"
-        v-model="dailyBudgetValue"
-        type="number"
-        placeholder="Set daily budget"
+        id="currency-input"
+        v-model="newCurrency"
+        type="text"
+        placeholder="Add new currency"
         class="col-span-10 rounded-r-none"
       />
-      <BaseButton class="col-span-2 border-l-0 rounded-l-none" @click="setDailyBudget(dailyBudgetValue)">
+      <BaseButton
+        class="col-span-2 border-l-0 rounded-l-none"
+        @click="saveNewCurrency(newCurrency)"
+      >
         <template #text>
-          <CheckIcon class="w-6 h-6" />
+          <PlusIcon class="w-6 h-6" />
         </template>
       </BaseButton>
     </div>
@@ -62,7 +68,7 @@ import { PlusIcon, CheckIcon } from '@heroicons/vue/24/outline';
 import BaseLayout from '@/components/layouts/BaseLayout/BaseLayout.vue';
 import BaseInput from '@/components/ui/controls/BaseInput/BaseInput.vue';
 import BaseButton from '@/components/ui/controls/BaseButton/BaseButton.vue';
-import BaseLabel from '@/components/ui/BaseLabel/BaseLabel.vue';
+import BaseCurrencyGroupItem from '@/components//BaseCurrencyGroupItem/BaseCurrencyGroupItem.vue';
 
 const settingsStore = useSettingsStore();
 
