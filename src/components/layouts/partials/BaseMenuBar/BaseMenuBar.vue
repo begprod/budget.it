1ee563e946ef3b8b71813d8002ee8e9501fdb6df
Andrey Aratov <beg431@gmail.com>
2024-01-29 00:05:41 +0700

    chore: update BaseMenuBar

--- src/components/layouts/partials/BaseMenuBar/BaseMenuBar.vue
+++ src/components/layouts/partials/BaseMenuBar/BaseMenuBar.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="fixed right-0 bottom-0 left-0 w-full px-6 py-3 bg-white border-t border-slate-200 z-[100]">
-    <div class="max-w-96 flex justify-between mx-auto">
+    <div class="max-w-96 flex justify-around mx-auto">
       <RouterLink to="/">
         <HomeIcon class="w-6 h-6 text-slate-400" />
       </RouterLink>
-      <RouterLink to="/dashboard">
+      <!-- <RouterLink to="/dashboard">
         <ChartBarIcon class="w-6 h-6 text-slate-400" />
-      </RouterLink>
+      </RouterLink> -->
       <RouterLink to="/settings">
         <Cog6ToothIcon class="w-6 h-6 text-slate-400" />
       </RouterLink>
