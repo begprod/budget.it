4b2a4d05339b155f25559f0653d185e07b7c80d0
Andrey Aratov <beg431@gmail.com>
2024-01-28 14:17:20 +0700

    chore: add BaseFormBar component

---
+++ src/components/BaseFormBar/BaseFormBar.vue
@@ -0,0 +1,19 @@
+<template>
+  <form class="relative overflow-hidden" @submit.prevent="submitHandler">
+    <div class="w-full">
+      <slot name="input" />
+    </div>
+
+    <div class="absolute top-0 right-0">
+      <slot name="button" />
+    </div>
+  </form>
+</template>
+
+<script setup lang="ts">
+const emit = defineEmits(['submit']);
+
+const submitHandler = () => {
+  emit('submit');
+};
+</script>
