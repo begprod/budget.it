154c1fe2c732d9060b7a066262da6b2d6c2de41f
Andrey Aratov <beg431@gmail.com>
2024-02-04 11:35:54 +0700

    fix: add radio button cursor pointer

--- src/components/ui/controls/BaseRadioButton/BaseRadioButton.vue
+++ src/components/ui/controls/BaseRadioButton/BaseRadioButton.vue
@@ -12,7 +12,7 @@
     <label
       :for="id"
       :class="additionalClasses"
-      class="block px-3 py-1 text-sm text-slate-400 border border-slate-300 rounded-2xl peer-checked/radio:text-white peer-checked/radio:bg-sky-400 select-none"
+      class="block px-3 py-1 text-sm text-slate-400 border border-slate-300 rounded-2xl peer-checked/radio:text-white peer-checked/radio:bg-sky-400 select-none cursor-pointer"
     >
       {{ label }}
     </label>
