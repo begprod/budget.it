f0ae81cea2c7fcc38cf6ed8f1e84b88bd84be7fd
Andrey Aratov <beg431@gmail.com>
2024-02-28 11:18:44 +0700

    chore: fix prettier

--- src/components/layouts/partials/BaseMenuBar/BaseMenuBar.vue
+++ src/components/layouts/partials/BaseMenuBar/BaseMenuBar.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="fixed right-0 bottom-0 left-0 w-full px-6 py-5 bg-white border-t border-slate-200 z-[100]">
+  <div
+    class="fixed right-0 bottom-0 left-0 w-full px-6 py-5 bg-white border-t border-slate-200 z-[100]"
+  >
     <div class="max-w-96 flex justify-around mx-auto">
       <RouterLink to="/">
         <HomeIcon class="w-6 h-6 text-slate-400" />
