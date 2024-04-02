a688adaa6dce0059d8c99b4f7ee1a2703db253bd
Andrey Aratov <beg431@gmail.com>
2024-04-02 13:34:52 +0700

    chore: add BaseRadioButton test

---
+++ src/components/ui/controls/BaseRadioButton/BaseRadioButton.spec.ts
@@ -0,0 +1,29 @@
+import { describe, it, expect } from 'vitest';
+import { mount } from '@vue/test-utils';
+import BaseRadioButton from '@/components/ui/controls/BaseRadioButton/BaseRadioButton.vue';
+
+describe('BaseRadioButton', () => {
+  const wrapper = mount(BaseRadioButton, {
+    props: {
+      id: 'test id',
+      name: 'test name',
+      value: 'test value',
+      checked: false,
+      additionalClasses: 1,
+    } as any,
+  });
+
+  it('should contain the correct props', () => {
+    expect(wrapper.props().id).toBe('test id');
+    expect(wrapper.props().name).toBe('test name');
+    expect(wrapper.props().value).toBe('test value');
+    expect(wrapper.props().checked).toBe(false);
+    expect(wrapper.props().additionalClasses).toBe('test-class');
+  });
+
+  it('should emit change event', async () => {
+    await wrapper.find('input').trigger('change');
+
+    expect(wrapper.emitted().change).toBeTruthy();
+  });
+});
