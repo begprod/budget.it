f40823484c1d71213206f00a8479bafa180cdbe0
Andrey Aratov <beg431@gmail.com>
2025-02-26 10:41:42 +0300

    refactor: BaseHeader component

--- src/components/layouts/partials/BaseHeader/BaseHeader.vue
+++ src/components/layouts/partials/BaseHeader/BaseHeader.vue
@@ -1,19 +1,15 @@
 <template>
-  <header class="sticky top-0 w-full bg-white z-[100]">
-    <div class="container mx-auto px-5 py-3 flex items-center justify-between select-none">
-      <div class="flex items-center">
-        <a href="/budget.it/" class="flex items-baseline font-bold">
-          <div class="logo-gradient gradient-animation text-lg lg:text-2xl">budget.it</div>
-          <sub class="logo-sub-gradient gradient-animation text-xs ml-2 bottom-0">
-            v{{ version }}
-          </sub>
-        </a>
-      </div>
+  <header class="header">
+    <div class="wrapper header__inner">
+      <a href="/budget.it/" class="logo">
+        <span class="logo-gradient gradient-animation logo__title">budget.it</span>
+        <span class="logo-sub-gradient gradient-animation logo__subtitle"> v{{ version }} </span>
+      </a>
 
-      <a href="https://github.com/begprod/budget.it" target="_blank" class="select-none">
+      <a class="header__github" href="https://github.com/begprod/budget.it" target="_blank">
         <img
           src="@/assets/images/github-mark.svg"
-          class="w-7 opacity-80 hover:opacity-100 transition-all duration-300"
+          class="icon icon_xl"
           width="40"
           height="40"
           alt="budget.it github"
@@ -28,7 +24,50 @@
 import { version } from '../../../../../package.json';
 </script>
 
-<style scoped lang="scss">
+<style scoped>
+.header {
+  position: sticky;
+  top: 0;
+  width: 100%;
+  background-color: var(--white);
+  z-index: 100;
+}
+
+.header__inner {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0.75rem 1.25rem;
+  user-select: none;
+}
+
+.logo {
+  display: flex;
+  align-items: baseline;
+  gap: 0.5rem;
+  font-weight: 700;
+}
+
+.logo__title {
+  font-size: clamp(var(--typo-size-lg), 2.34vw, var(--typo-size-2xl));
+}
+
+.logo__subtitle {
+  font-size: 0.75rem;
+}
+
+.header__github {
+  user-select: none;
+  opacity: 0.8;
+  transition: 0.3s ease-in-out;
+  transition-property: opacity, transform;
+
+  &:hover {
+    opacity: 1;
+    transform: scale(1.1);
+  }
+}
+
 .logo-gradient {
   background-color: #0093e9;
   background-image: linear-gradient(43deg, #85ffbd 0%, #0093e9 46%, #00dbde 100%);
@@ -55,19 +94,5 @@ import { version } from '../../../../../package.json';
 
 .gradient-animation {
   animation: gradient 15s ease-in-out infinite;
-}
-
-@keyframes gradient {
-  0% {
-    background-position: 0% 50%;
-  }
-
-  50% {
-    background-position: 100% 50%;
-  }
-
-  100% {
-    background-position: 0% 50%;
-  }
 }
 </style>
