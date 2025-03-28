f5b39f4555d5a4a92b266b39353c75ae3bc84aa7
Andrey Aratov <beg431@gmail.com>
2025-03-28 09:49:33 +0300

    refactor: BaseButton component

--- src/components/ui/controls/BaseButton/BaseButton.vue
+++ src/components/ui/controls/BaseButton/BaseButton.vue
@@ -1,11 +1,5 @@
 <template>
-  <button
-    class="flex items-center justify-center px-3 py-2 w-full text-sm text-sky-400 bg-white border border-slate-300 rounded-xl focus:outline-none focus:border-sky-400 lg:hover:bg-sky-500 lg:hover:text-white disabled:opacity-50 disabled:bg-slate-200 transition-[background-color transform] duration-300"
-    :type="type"
-    :title="title"
-    :disabled="isDisabled"
-    @click="clickHandler"
-  >
+  <button class="button" :type="type" :title="title" :disabled="isDisabled" @click="clickHandler">
     <slot name="leftIcon" />
     <slot name="text" />
     <slot name="rightIcon" />
@@ -29,3 +23,39 @@ const clickHandler = () => {
   emits('click');
 };
 </script>
+
+<style scoped>
+.button {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 0.5rem;
+  width: 100%;
+  padding: 0.5rem 0.75rem;
+  font-size: var(--typo-size-sm);
+  line-height: 1.5;
+  color: var(--blue-400);
+  border: 1px solid var(--slate-300);
+  border-radius: 0.75rem;
+  background-color: var(--white);
+  transition: 0.3s ease-in-out;
+  transition-property: background-color, opacity, box-shadow;
+
+  &:hover {
+    box-shadow: 0 2px 3px -1px rgba(0, 0, 0, 0.2);
+  }
+
+  &:focus {
+    outline: none;
+    border-color: var(--slate-400);
+  }
+
+  &:disabled {
+    border-color: var(--slate-300);
+    box-shadow: none;
+    cursor: not-allowed;
+    background-color: var(--slate-200);
+    opacity: 0.5;
+  }
+}
+</style>
