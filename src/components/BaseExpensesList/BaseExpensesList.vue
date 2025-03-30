7360703dda42315d58a2015227af7b7182a8ec72
Andrey Aratov <beg431@gmail.com>
2025-03-30 17:18:03 +0300

    refactor: BaseExpenseList component

--- src/components/BaseExpensesList/BaseExpensesList.vue
+++ src/components/BaseExpensesList/BaseExpensesList.vue
@@ -1,67 +1,55 @@
 <template>
-  <div
-    v-if="month"
-    class="sticky top-[52px] flex flex-col py-5 px-5 gradient lg:!bg-none lg:bg-white text-white lg:text-slate-700 rounded-tl-3xl rounded-tr-3xl z-40"
-  >
-    <div class="mb-2">
-      <div class="mb-2 text-xl" data-test-id="month-title">{{ month.name }}</div>
-      <div class="text-4xl font-bold" data-test-id="monthly-expenses">
-        {{ monthExpenses }}
-      </div>
-    </div>
-    <div class="w-full">
-      <div class="flex justify-between text-xs">
-        <div class="flex">
-          <div class="mr-2 font-bold">Monthly budget:</div>
-          <div data-test-id="monthly-budget">
-            {{ monthDays.length * dailyBudget }}
-          </div>
+  <div v-if="month" class="stats-panel gradient">
+    <div class="stats-panel__inner">
+      <div class="stats-panel__header">
+        <div class="stats-panel__title" data-test-id="month-title">{{ month.name }}</div>
+        <div class="stats-panel__overall" data-test-id="monthly-expenses">
+          {{ monthExpenses }}
         </div>
-        <div class="font-bold" data-test-id="monthly-percents">
+      </div>
+      <div class="stats-panel__footer">
+        <div data-test-id="monthly-budget">Monthly budget: {{ overAllExpneses }}</div>
+
+        <div data-test-id="monthly-percents">
           {{ countProgressPercentage(month.id, dailyBudget) }}%
         </div>
       </div>
+
       <BaseProgressBar :percentage="countProgressPercentage(month.id, dailyBudget)" />
     </div>
   </div>
 
-  <div v-if="month" class="relative grid gap-3 p-5 z-0">
+  <div v-if="month" class="expense-list">
     <template v-for="day in monthDays" :key="day.id">
-      <div v-if="!day.isFuture" class="relative">
-        <div
-          class="sticky top-[200px] flex flex-col items-start py-3 bg-white font-bold select-none z-40"
-        >
-          <div class="flex">
-            <div class="flex flex-col">
-              <div class="flex items-center text-sm lg:text-base" data-test-id="day-title">
-                {{ day.number }} {{ day.name }}
-                <div
-                  v-if="day.isCurrent"
-                  data-test-id="current-day-indicator"
-                  class="shrink-0 w-2 h-2 ml-2 rounded-full bg-green-500 select-none animate-pulse"
-                />
-              </div>
-            </div>
+      <div v-if="!day.isFuture" class="expense-list__inner">
+        <div class="expense-list__header" data-test-id="day-title">
+          <div class="expense-list__date">
+            {{ day.number }} {{ day.name }}
+
+            <div
+              v-if="day.isCurrent"
+              data-test-id="current-day-indicator"
+              class="icon icon_xs expense-list__indicator animate-pulse"
+            />
           </div>
 
           <div
-            class="text-xs lg:text-sm"
+            class="expense-list__limit"
             :class="{
-              'text-emerald-500': getDailyExpenses(day.id) <= dailyBudget,
-              'text-rose-500': getDailyExpenses(day.id) > dailyBudget,
-              hidden: getDailyExpenses(day.id) === 0,
+              'expense-list__limit_exceeded': getDailyExpenses(day.id) > dailyBudget,
+              'visually-hidden': getDailyExpenses(day.id) === 0,
             }"
             data-test-id="daily-expenses"
           >
-            {{ getDailyExpenses(day.id) }} /
-            {{ dailyBudget }}
+            {{ getDailyExpenses(day.id) }} / {{ dailyBudget }}
           </div>
         </div>
 
         <div
           v-for="(expenseItem, index) in expenses[day.id]"
           :key="index"
-          class="flex flex-wrap gap-2"
+          class="expense-list__expenses"
+          :class="{ 'expense-list__expenses_past': !day.isCurrent }"
         >
           <TransitionGroup name="list">
             <template v-for="expense in expenseItem" :key="expense.id">
@@ -69,13 +57,12 @@
                 :createdAt="expense.createdAt"
                 :value="expense.value"
                 :currency="expense.currency"
-                :class="{ 'opacity-30': !day.isCurrent }"
                 @delete-item="removeItem(expense.id, day.id)"
               />
             </template>
           </TransitionGroup>
 
-          <div v-if="!expenses[day.id].items.length" class="flex items-center w-full">
+          <div v-if="!expenses[day.id].items.length" class="expense-list__empty-message">
             <BaseEmptyListMessage>
               <template #icon>
                 <Coins class="icon icon_sm" />
@@ -84,7 +71,7 @@
             </BaseEmptyListMessage>
           </div>
 
-          <div v-if="day.isCurrent" class="absolute bottom-0 w-full" data-test-id="input-slot">
+          <div v-if="day.isCurrent" class="expense-list__input" data-test-id="input-slot">
             <Transition>
               <slot name="input" />
             </Transition>
@@ -101,6 +88,7 @@ import BaseEmptyListMessage from '@/components/ui/BaseEmptyListMessage/BaseEmpty
 import BaseExpense from '@/components/BaseExpense/BaseExpense.vue';
 import BaseProgressBar from '@/components/ui/BaseProgressBar/BaseProgressBar.vue';
 import { Coins } from 'lucide-vue-next';
+import { computed } from 'vue';
 
 interface IProps {
   month: IMonth;
@@ -143,43 +131,130 @@ const getDailyExpenses = (dayId: IDay['id']) => {
   return dayExpensesCounter;
 };
 
+const overAllExpneses = computed(() => props.monthDays.length * props.dailyBudget);
+
 const removeItem = (id: string, dayId: string) => {
   emit('remove-item', id, dayId);
 };
 </script>
 
-<style scoped lang="scss">
-.gradient {
-  background-image: linear-gradient(180deg, #19a4f8 0%, #006396 100%);
+<style scoped>
+.stats-panel {
+  position: sticky;
+  top: 52px;
+  display: flex;
+  flex-direction: column;
+  padding: 1.25rem;
+  color: var(--slate-700);
+  background-color: var(--white);
+  border-top-left-radius: 1.5rem;
+  border-top-right-radius: 1.5rem;
+  z-index: 40;
 }
 
-.v-enter-active,
-.v-leave-active {
-  transform: translateY(0);
-  transition: all 0.5s ease;
+.stats-panel__inner {
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
 }
 
-.v-enter-from,
-.v-leave-to {
-  transform: translateY(100%);
-  opacity: 0;
+.stats-panel__header,
+.stats-panel__footer {
+  display: flex;
+  flex-direction: column;
+  gap: 0.3rem;
 }
 
-.list-move,
-.list-enter-active,
-.list-leave-active {
-  transition: all 0.5s ease;
+.stats-panel__footer {
+  flex-direction: row;
+  justify-content: space-between;
+  font-size: var(--typo-size-xs);
 }
 
-.list-enter-from,
-.list-leave-to {
-  opacity: 0;
-  transform: translateX(30px) scale(0.5);
-  transition: all 0.5s ease;
+.stats-panel__title {
+  font-size: var(--typo-size-xl);
 }
 
-.list-leave-active {
+.stats-panel__overall {
+  line-height: 1;
+  font-size: var(--typo-size-4xl);
+  font-weight: bold;
+}
+
+.expense-list {
+  position: relative;
+  display: grid;
+  gap: 0.75rem;
+  padding: 1rem 1.25rem;
+  z-index: 0;
+}
+
+.expense-list__inner {
+  position: relative;
+}
+
+.expense-list__header {
+  position: sticky;
+  top: 200px;
+  display: flex;
+  flex-direction: column;
+  padding: 0.5rem 0;
+  font-weight: bold;
+  font-size: clamp(var(--typo-size-sm), 1.56vw, var(--typo-size-base));
+  background-color: var(--white);
+  user-select: none;
+  z-index: 40;
+}
+
+.expense-list__date {
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+}
+
+.expense-list__indicator {
+  border-radius: 100%;
+  background-color: var(--green-500);
+}
+
+.expense-list__limit {
+  color: var(--green-500);
+  font-size: clamp(var(--typo-size-xs), 1.56vw, var(--typo-size-base));
+}
+
+.expense-list__limit_exceeded {
+  color: var(--red-400);
+}
+
+.expense-list__expenses {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 0.5rem;
+}
+
+.expense-list__expenses_past {
+  opacity: 0.3;
+}
+
+.expense-list__input {
   position: absolute;
-  transition: all 0.5s ease;
+  bottom: 0;
+  width: 100%;
+}
+
+.expense-list__empty-message {
+  display: flex;
+  align-items: center;
+  width: 100%;
+}
+
+@media screen and (max-width: 1024px) {
+  .stats-panel {
+    color: var(--white);
+  }
+
+  .gradient {
+    background-image: linear-gradient(180deg, #19a4f8 0%, #006396 100%);
+  }
 }
 </style>
